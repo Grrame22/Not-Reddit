2ca5288915edeceaafca5c79b950ac531ec49dd1
Grrame22 <grrame22@gmail.com>
2020-08-26 22:00:04 +0300

    Added new category "Other"

--- startup/forms.py
+++ startup/forms.py
@@ -1,6 +1,4 @@
 from django import forms
-from django.contrib.auth.models import User
-from django.core.exceptions import ValidationError
 
 from .models import Article, UserProfile, Comments
 from snowpenguin.django.recaptcha3.fields import ReCaptchaField
